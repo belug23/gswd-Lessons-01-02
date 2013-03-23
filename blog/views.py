8600bed3eb935a29ee0b6d11ae3c6a248750152e
Alexandre Lessard <alexandre.lessard.ti@gmail.com>
2013-03-23 18:25:00 +0000

    Added South, made a blog and finished the lesson

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+class PublishedPostsMixin(object):
+    def get_queryset(self):
+        return self.model.objects.live()
+
+class PostListView(PublishedPostsMixin, ListView):
+    model = Post
+
+
+class PostDetailView(PublishedPostsMixin, DetailView):
+    model = Post
